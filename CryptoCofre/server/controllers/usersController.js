4d5eb2948d95d2e6e092cff308afb434501450ba
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2017-11-21 23:14:11 -0300

    passando para async

--- CryptoCofre/server/controllers/usersController.js
+++ CryptoCofre/server/controllers/usersController.js
@@ -1,28 +1,53 @@
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 const User = mongoose.model('User');
 
-exports.get = (req, res, next) => {
-    res.status(201).send(req.body);
-};
+// exports.get = (req, res, next) => {
+//     res.status(201).send(req.body);
+// };
 
-exports.post = (req, res, next) => {
-    let user = new User(req.body);
-    user.save().then(e => {
+exports.get = async(req, res, next) => {
+    try {
+        res.status(200).send({aqui:'chegou'});
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+}
+
+exports.post = async(req, res, next) => {
+    try {
+        let user = new User(req.body);
+        user.save();
         res.status(201).send({
             mensage: 'Usuário cadastrado!'
-        }).catch( err => {
-            res.status(400).send({error: err})
         });
-    })
-};
+    } catch (e) {
+        res.status(500).send({
+            message: e.message
+        });
+    }
+}
 
 
-exports.put = (req, res, next) => {
-    res.status(200).send(req.body);
-};
+// exports.post = (req, res, next) => {
+//     let user = new User(req.body);
+//     user.save().then(e => {
+//         res.status(201).send({
+//             mensage: 'Usuário cadastrado!'
+//         }).catch( err => {
+//             res.status(400).send({error: err})
+//         });
+//     })
+// };
 
 
-exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
-};
+// exports.put = (req, res, next) => {
+//     res.status(200).send(req.body);
+// };
+
+
+// exports.delete = (req, res, next) => {
+//     res.status(200).send(req.body);
+// };
