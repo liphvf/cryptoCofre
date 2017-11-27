0c6c49b0248b01716ca8d651f407b9d85e89086a
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2017-11-27 01:33:30 -0300

    get all

--- CryptoCofre/server/controllers/usersController.js
+++ CryptoCofre/server/controllers/usersController.js
@@ -1,35 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // mongoose.Promise = global.Promise;
-const User = mongoose.model('User');
+const User = mongoose.model("User");
 
 // exports.get = (req, res, next) => {
 //     res.status(201).send(req.body);
 // };
 
-exports.get = async(req, res, next) => {
-    try {
-        res.status(200).send({aqui:'chegou'});
-    } catch (e) {
-        res.status(500).send({
-            message: 'Falha ao processar sua requisição'
-        });
-    }
-}
+exports.get = async (req, res, next) => {
+  const result = User.find({}, function(err, users) {
+    var userList = {};
 
-exports.post = async(req, res, next) => {
-    try {
-        let user = new User(req.body);
-        user.save();
-        res.status(201).send({
-            mensage: 'Usuário cadastrado!'
-        });
-    } catch (e) {
-        res.status(500).send({
-            message: e.message
-        });
-    }
-}
+    users.forEach(function(user) {
+      userList[user._id] = user;
+    });
+    
+    res.send(userList);
+  });
+  // res.status(200).send(result);
+  // return result;
+  // try {
+  //   var test = user.find({});
+  //   res.status(200).send(test);
+  // } catch (e) {
+  //   res.status(500).send({
+  //     message: "Falha ao processar sua requisição"
+  //   });
+  // }
+};
 
+exports.post = async (req, res, next) => {
+  try {
+    let user = new User(req.body);
+    user.save();
+    res.status(201).send({
+      mensage: "Usuário cadastrado!"
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: e.message
+    });
+  }
+};
 
 // exports.post = (req, res, next) => {
 //     let user = new User(req.body);
@@ -42,11 +53,9 @@ exports.post = async(req, res, next) => {
 //     })
 // };
 
-
 // exports.put = (req, res, next) => {
 //     res.status(200).send(req.body);
 // };
-
 
 // exports.delete = (req, res, next) => {
 //     res.status(200).send(req.body);
