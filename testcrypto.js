0f192c9e32b11ca5b10e8537d479dd9df3b6dbc1
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2017-11-26 23:40:11 -0300

    +++

--- testcrypto.js
+++ testcrypto.js
@@ -52,30 +52,25 @@ const crypto = require("crypto");
 
 // console.log(hash);
 
-
-
-
 // var crypto = require('crypto'),
-var algorithm = 'aes-256-ctr';
-var password = 'xuxu';
+var algorithm = "aes-128-ecb";
+var password = "xuxu";
 
-function encrypt(text){
-
-var cipher = crypto.createCipher(algorithm,password)
-var crypted = cipher.update(text,'utf8','hex')
-crypted += cipher.final('hex');
-return crypted;
-
+function encrypt(text) {
+  var cipher = crypto.createCipher(algorithm, password);
+  var crypted = cipher.update(text, "utf8", "hex");
+  crypted += cipher.final("hex");
+  return crypted;
 }
 
-function decrypt(text){
-var decipher = crypto.createDecipher(algorithm,password)
-var dec = decipher.update(text,'hex','utf8')
-dec += decipher.final('utf8');
-return dec;
+function decrypt(text) {
+  var decipher = crypto.createDecipher(algorithm, password);
+  var dec = decipher.update(text, "hex", "utf8");
+  dec += decipher.final("utf8");
+  return dec;
 }
 
-var hw = encrypt("hello world")
+var hw = encrypt("xuxu");
 console.log(hw);
 // outputs hello world
 console.log(decrypt(hw));
